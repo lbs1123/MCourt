6eb6faf40ed9332d351871af30c4053774d28afe
lee byung sik <lbs1123_@naver.com>
2017-09-15 15:34:39 +0900

    액티비트 6개 추가

---
+++ app/src/main/java/com/shinhan/mcourt/IssueActivity.java
@@ -0,0 +1,13 @@
+package com.shinhan.mcourt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IssueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_issue);
+    }
+}
