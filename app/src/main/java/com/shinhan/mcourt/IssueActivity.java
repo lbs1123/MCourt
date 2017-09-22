d7172cdcfbfbadc5375f530a89404cfccb5fc98f
lee byung sik <lbs1123_@naver.com>
2017-09-22 17:23:19 +0900

    9.22 두번째 파일

--- app/src/main/java/com/shinhan/mcourt/IssueActivity.java
+++ app/src/main/java/com/shinhan/mcourt/IssueActivity.java
@@ -1,7 +1,9 @@
 package com.shinhan.mcourt;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class IssueActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class IssueActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_issue);
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(IssueActivity.this, MainActivity.class);
+        startActivity(intent);
+
     }
 }
--- app/src/main/java/com/shinhan/mcourt/LitigantActivity.java
+++ app/src/main/java/com/shinhan/mcourt/LitigantActivity.java
@@ -1,7 +1,9 @@
 package com.shinhan.mcourt;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class LitigantActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class LitigantActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_litigant);
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(LitigantActivity.this, IssueActivity.class);
+        startActivity(intent);
     }
 }
